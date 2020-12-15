db41aa662aecd94e6d474f91537a5f7879867341
Raghu Kaippully <rkaippully@gmail.com>
2020-12-15 23:18:54 +0530

    day 14 and 15

--- 2020/src/Day14.hs
+++ 2020/src/Day14.hs
@@ -3,8 +3,86 @@ module Day14
   , day14Part2
   ) where
 
+import Control.Monad
+import Data.Bits
+import Data.Foldable
+import qualified Data.Map.Strict as M
+import Data.Maybe
+import Data.Monoid
+import Data.Void
+import Text.Megaparsec
+import Text.Megaparsec.Char
+
+type Addr = Integer
+type Value = Integer
+
+data Bit = X | One | Zero
+
+data Insn = SetMem Addr Value
+          | SetMask [(Int, Bit)]
+
+parseProgram :: String -> [Insn]
+parseProgram = map (fromJust . parseMaybe insn) . lines
+
+type Parser = Parsec Void String
+
+insn :: Parser Insn
+insn = setMem <|> setMask
+
+setMem :: Parser Insn
+setMem = do
+  addr <- string "mem[" *> number
+  val <- string "] = " *> number
+  pure $ SetMem addr val
+
+setMask :: Parser Insn
+setMask = do
+  s <- string "mask = " *> many alphaNumChar
+  pure $ SetMask $ fst $ foldl' f ([], 35) s
+
+  where
+    f :: ([(Int, Bit)], Int) -> Char -> ([(Int, Bit)], Int)
+    f (bs, n) 'X' = ((n, X):bs, n-1)
+    f (bs, n) '1' = ((n, One):bs, n-1)
+    f (bs, n) '0' = ((n, Zero):bs, n-1)
+    f _ _         = error "bad bitmask"
+
+number :: Parser Integer
+number = read <$> some digitChar <* space
+
+runProgram1 :: [Insn] -> Integer
+runProgram1 = getSum . foldMap Sum . fst . foldl' exec (M.empty, id)
+  where
+    exec :: (M.Map Addr Value, Value -> Value) -> Insn -> (M.Map Addr Value, Value -> Value)
+    exec (m, g) (SetMem addr val) = (M.insert addr (g val) m, g)
+    exec (m, _) (SetMask bs)      = (m, valueTrans bs)
+
+    valueTrans :: [(Int, Bit)] -> (Value -> Value)
+    valueTrans = appEndo . foldMap f
+
+    f :: (Int, Bit) -> Endo Value
+    f = Endo . \case
+      (_, X)    -> id
+      (i, One)  -> (`setBit` i)
+      (i, Zero) -> (`clearBit` i)
+
+runProgram2 :: [Insn] -> Integer
+runProgram2 = getSum . foldMap Sum . fst . foldl' exec (M.empty, pure)
+  where
+    exec :: (M.Map Addr Value, Addr -> [Addr]) -> Insn -> (M.Map Addr Value, Addr -> [Addr])
+    exec (m, g) (SetMem addr val) = (foldl' (\m' addr' -> M.insert addr' val m') m (g addr), g)
+    exec (m, _) (SetMask bs)      = (m, addrTrans bs)
+
+    addrTrans :: [(Int, Bit)] -> (Addr -> [Addr])
+    addrTrans = foldl' f pure
+
+    f :: (Addr -> [Addr]) -> (Int, Bit) -> (Addr -> [Addr])
+    f g (_, Zero) = g
+    f g (i, One)  = g >=> \a -> [a `setBit` i]
+    f g (i, X)    = g >=> \a -> [a `setBit` i, a `clearBit` i]
+
 day14Part1 :: String -> String
-day14Part1 = undefined
+day14Part1 = show . runProgram1 . parseProgram
 
 day14Part2 :: String -> String
-day14Part2 = undefined
+day14Part2 = show . runProgram2 . parseProgram
--- 2020/src/Day15.hs
+++ 2020/src/Day15.hs
@@ -3,8 +3,40 @@ module Day15
   , day15Part2
   ) where
 
+import Control.Monad
+import Control.Monad.ST
+import Data.Vector.Unboxed.Mutable (MVector)
+import qualified Data.Vector.Unboxed.Mutable as V
+
+type Number = Int
+type Turn = Int
+
+input :: Turn -> String -> ST s (Number, Turn, MVector s Turn)
+input limit s = do
+  let start = zip (read $ "[" <> s <> "]") [1..]
+
+  arr <- V.replicate limit (-1)
+  forM_ start $ uncurry (V.write arr)
+
+  let (n, t) = last start
+  pure (n, t, arr)
+
+turns :: Turn -> (Number, Turn, MVector s Turn) -> ST s Number
+turns limit = go
+  where
+    go :: (Number, Turn, MVector s Turn) -> ST s Number
+    go (num, turn, arr) | turn == limit = pure num
+                        | otherwise     = do
+                            t <- arr `V.read` num
+                            let nextNum = if t == -1 then 0 else turn - t
+                            V.write arr num turn
+                            go (nextNum, turn+1, arr)
+
+memoryGame :: Turn -> String -> String
+memoryGame t s = show $ runST (input t s >>= turns t)
+
 day15Part1 :: String -> String
-day15Part1 = undefined
+day15Part1 = memoryGame 2020
 
 day15Part2 :: String -> String
-day15Part2 = undefined
+day15Part2 = memoryGame 30000000
