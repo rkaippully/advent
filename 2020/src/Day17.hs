3841b9bb13cf4012585c34237f6fbe235bbdac67
Raghu Kaippully <rkaippully@gmail.com>
2020-12-19 16:02:06 +0530

    refactor day 17

--- 2020/src/Day17.hs
+++ 2020/src/Day17.hs
@@ -7,53 +7,55 @@ import Data.Function
 import Data.Set (Set)
 import qualified Data.Set as Set
 
-
 type Coord3D = (Int, Int, Int)
 type Coord4D = (Int, Int, Int, Int)
 
-to3DCoords :: String -> Set Coord3D
-to3DCoords = mconcat . map lineToCoords . zip [0..] . lines
-  where
-    lineToCoords :: (Int, String) -> Set Coord3D
-    lineToCoords (x, s) = Set.fromList $ map (\(y, _) -> (x, y, 0)) $ filter ((== '#') . snd) $ zip [0..] s
+class Coord a where
+  toCoords :: String -> Set a
+  neighbours :: a -> Set a
 
-to4DCoords :: String -> Set Coord4D
-to4DCoords = Set.map (\(x, y, z) -> (x, y, z, 0)) . to3DCoords
+instance Coord Coord3D where
+  toCoords :: String -> Set Coord3D
+  toCoords = mconcat . zipWith (curry lineToCoords) [0..] . lines
+    where
+      lineToCoords :: (Int, String) -> Set Coord3D
+      lineToCoords (x, s) = Set.fromList $ map (\(y, _) -> (x, y, 0)) $ filter ((== '#') . snd) $ zip [0..] s
 
-nextCycle :: Ord a => (a -> Set a) -> Set a -> Set a
-nextCycle neighbours world = Set.filter (isActive neighbours world) possibles
-  where
-    possibles = Set.unions $ Set.map (Set.insert <*> neighbours) world
-
-neighbours4d :: Coord4D -> Set Coord4D
-neighbours4d (x, y, z, w) = Set.fromList [(x', y', z', w') | x' <- [x-1,x,x+1]
-                                                         , y' <- [y-1,y,y+1]
-                                                         , z' <- [z-1,z,z+1]
-                                                         , w' <- [w-1,w,w+1]
-                                                         , (x, y, z, w) /= (x', y', z', w')]
-
-neighbours3d :: Coord3D -> Set Coord3D
-neighbours3d (x, y, z) = Set.fromList [(x', y', z') | x' <- [x-1,x,x+1]
+  neighbours :: Coord3D -> Set Coord3D
+  neighbours (x, y, z) = Set.fromList [(x', y', z') | x' <- [x-1,x,x+1]
                                                     , y' <- [y-1,y,y+1]
                                                     , z' <- [z-1,z,z+1]
                                                     , (x, y, z) /= (x', y', z')]
 
-isActive :: Ord a => (a -> Set a) -> Set a -> a -> Bool
-isActive neighbours world c | c `Set.member` world = cnt == 2 || cnt == 3
-                            | otherwise = cnt == 3
+instance Coord Coord4D where
+  toCoords :: String -> Set Coord4D
+  toCoords = Set.map (\(x, y, z) -> (x, y, z, 0)) . toCoords
+
+  neighbours :: Coord4D -> Set Coord4D
+  neighbours (x, y, z, w) = Set.fromList [(x', y', z', w') | x' <- [x-1,x,x+1]
+                                                           , y' <- [y-1,y,y+1]
+                                                           , z' <- [z-1,z,z+1]
+                                                           , w' <- [w-1,w,w+1]
+                                                           , (x, y, z, w) /= (x', y', z', w')]
+
+nextCycle :: (Ord a, Coord a) => Set a -> Set a
+nextCycle world = Set.filter (isActive world) possibles
+  where
+    possibles = Set.unions $ Set.map (Set.insert <*> neighbours) world
+
+isActive :: (Ord a, Coord a) => Set a -> a -> Bool
+isActive world c | c `Set.member` world = cnt == 2 || cnt == 3
+                 | otherwise = cnt == 3
   where
     cnt = Set.size $ Set.intersection (neighbours c) world
 
+solve :: (Ord a, Coord a) => String -> Set a
+solve s = toCoords s
+          & iterate nextCycle
+          & (!!6)
+
 day17Part1 :: String -> String
-day17Part1 s = to3DCoords s
-               & iterate (nextCycle neighbours3d)
-               & (!!6)
-               & Set.size
-               & show
+day17Part1 = show . Set.size . solve @Coord3D
 
 day17Part2 :: String -> String
-day17Part2 s = to4DCoords s
-               & iterate (nextCycle neighbours4d)
-               & (!!6)
-               & Set.size
-               & show
+day17Part2 = show . Set.size . solve @Coord4D
