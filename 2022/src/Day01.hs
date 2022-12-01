186e1038d8a2a37c2645ce404d3be9ad9df12cd0
Raghu Kaippully <rkaippully@gmail.com>
2022-12-01 12:37:54 +0530

    Day 1

--- 2022/src/Day01.hs
+++ 2022/src/Day01.hs
@@ -3,10 +3,20 @@ module Day01 (
   day01Part2,
 ) where
 
-import Relude
+import Data.List (groupBy, sort)
+import Data.Ord (Down (..))
 
 day01Part1 :: String -> String
-day01Part1 = error "not implemented"
+day01Part1 = show . head . sortedCalories
 
 day01Part2 :: String -> String
-day01Part2 = error "not implemented"
+day01Part2 = show . sum . take 3 . sortedCalories
+
+sortedCalories :: String -> [Integer]
+sortedCalories =
+  map getDown
+    . sort @(Down Integer)
+    . map (sum . map (Down . read))
+    . filter (/= [""])
+    . groupBy (\a b -> a /= "" && b /= "")
+    . lines
