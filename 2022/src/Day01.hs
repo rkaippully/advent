a584125bc17f26ef0471ba045ef93e849d8b064e
Raghu Kaippully <rkaippully@gmail.com>
2022-12-02 08:42:26 +0530

    Simplify day 1

--- 2022/src/Day01.hs
+++ 2022/src/Day01.hs
@@ -3,8 +3,8 @@ module Day01 (
   day01Part2,
 ) where
 
-import Data.List (groupBy, sort)
-import Data.Ord (Down (..))
+import Data.List (groupBy, sortOn)
+import Data.Ord (Down (Down))
 
 day01Part1 :: String -> String
 day01Part1 = show . head . sortedCalories
@@ -14,9 +14,8 @@ day01Part2 = show . sum . take 3 . sortedCalories
 
 sortedCalories :: String -> [Integer]
 sortedCalories =
-  map getDown
-    . sort @(Down Integer)
-    . map (sum . map (Down . read))
+  sortOn Down
+    . map (sum . map read)
     . filter (/= [""])
     . groupBy (\a b -> a /= "" && b /= "")
     . lines
