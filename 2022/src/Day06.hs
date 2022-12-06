99f406e16872f3f9d35d9f5cfa3ca7ea9a6f61a2
Raghu Kaippully <rkaippully@gmail.com>
2022-12-06 10:54:37 +0530

    day 6

--- 2022/src/Day06.hs
+++ 2022/src/Day06.hs
@@ -3,9 +3,23 @@ module Day06 (
   day06Part2,
 ) where
 
+import qualified Data.Set as Set
+
+uniqueCharIdx :: Int -> [Char] -> Int
+uniqueCharIdx count = go . zip [0 ..]
+  where
+    go :: [(Int, Char)] -> Int
+    go [] = error "Marker not found"
+    go xs@((n, _) : _) =
+      let
+        chars = Set.fromList $ map snd $ take count xs
+       in
+        if Set.size chars == count
+          then n + count
+          else go (tail xs)
 
 day06Part1 :: String -> String
-day06Part1 = error "not implemented"
+day06Part1 = show . uniqueCharIdx 4
 
 day06Part2 :: String -> String
-day06Part2 = error "not implemented"
+day06Part2 = show . uniqueCharIdx 14
