e8fe6a2706fd01a6b0be7ef2c8a6bbf0a3df59de
Raghu Kaippully <rkaippully@gmail.com>
2022-12-07 10:04:56 +0530

    Use tails to improve day 6 solution

--- 2022/src/Day06.hs
+++ 2022/src/Day06.hs
@@ -3,20 +3,17 @@ module Day06 (
   day06Part2,
 ) where
 
+import Data.Function ((&))
+import Data.List (tails)
 import qualified Data.Set as Set
 
 uniqueCharIdx :: Int -> [Char] -> Int
-uniqueCharIdx count = go . zip [0 ..]
-  where
-    go :: [(Int, Char)] -> Int
-    go [] = error "Marker not found"
-    go xs@((n, _) : _) =
-      let
-        chars = Set.fromList $ map snd $ take count xs
-       in
-        if Set.size chars == count
-          then n + count
-          else go (tail xs)
+uniqueCharIdx count s =
+  tails s
+    & zipWith (\i cs -> (i, Set.fromList (take count cs))) [count ..]
+    & filter (\(_, cs) -> Set.size cs == count)
+    & head
+    & fst
 
 day06Part1 :: String -> String
 day06Part1 = show . uniqueCharIdx 4
