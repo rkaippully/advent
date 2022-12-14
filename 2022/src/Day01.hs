bba02631d9aca1206f0bac707b26a358bc6730f4
Raghu Kaippully <rkaippully@gmail.com>
2022-12-14 20:31:51 +0530

    Use Data.List.Split

--- 2022/src/Day01.hs
+++ 2022/src/Day01.hs
@@ -3,7 +3,8 @@ module Day01 (
   day01Part2,
 ) where
 
-import Data.List (groupBy, sortOn)
+import Data.List (sortOn)
+import Data.List.Split (splitWhen)
 import Data.Ord (Down (Down))
 
 day01Part1 :: String -> String
@@ -13,9 +14,4 @@ day01Part2 :: String -> String
 day01Part2 = show . sum . take 3 . sortedCalories
 
 sortedCalories :: String -> [Integer]
-sortedCalories =
-  sortOn Down
-    . map (sum . map read)
-    . filter (/= [""])
-    . groupBy (\a b -> a /= "" && b /= "")
-    . lines
+sortedCalories = sortOn Down . map (sum . map read) . splitWhen (== "") . lines
